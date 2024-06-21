f309e6aaa7565edf519386a3bf3c384b0c87d73b
KayoticCarnige <kayos156official@gmail.com>
2024-06-21 01:33:37 -0400

    Setup done???

---
+++ src/main/java/com/kckarnige/slowbronze/SlowBronzeClient.java
@@ -0,0 +1,9 @@
+package com.kckarnige.slowbronze;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SlowBronzeClient implements ClientModInitializer {
+    public void onInitializeClient() {
+        
+    }
+}
